a22841adcd7f6efa05bd61736e9dbc8548d268a4
alitacodes <techalita7070@gmail.com>
2025-06-03 09:51:17 +0530

    hello penguin

---
+++ test/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std::cout << "Hello Penguin from Sneha" << std::endl;
+    // cout << "Hello Penguin from Sneha" << endl;
+    cout << "Hello Penguin from Sneha";
+
+    
+}
